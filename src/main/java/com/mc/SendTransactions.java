31b6cebdd2df9bdab12f8d69f04ae43b6f95a7b8
Steve Womack <stwomack@gmail.com>
2016-11-22 10:29:06 -0600

    yolo

--- src/main/java/com/mc/SendTransactions.java
+++ src/main/java/com/mc/SendTransactions.java
@@ -1,5 +1,6 @@
 package com.mc;
 
+import org.apache.commons.io.FileUtils;
 import org.springframework.cloud.stream.annotation.EnableBinding;
 import org.springframework.cloud.stream.messaging.Source;
 import org.springframework.context.annotation.Bean;
@@ -8,6 +9,7 @@ import org.springframework.integration.annotation.Poller;
 import org.springframework.integration.core.MessageSource;
 import org.springframework.messaging.support.GenericMessage;
 
+import java.io.File;
 import java.io.IOException;
 
 @EnableBinding(Source.class)
@@ -18,6 +20,8 @@ public class SendTransactions {
     @Bean
     @InboundChannelAdapter(value = Source.OUTPUT, poller = @Poller(fixedDelay = "${fixedDelay}", maxMessagesPerPoll = "${messagesPerPoll}"))
     public MessageSource<String> send() throws IOException {
-        return () -> new GenericMessage<>("hi");
+        String message = FileUtils.readFileToString(new File("payload.csv"));
+        System.out.println(message);
+        return () -> new GenericMessage<>(message);
     }
 }
